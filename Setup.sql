2074a5a8cfb1594b3b97397d843c65dd63d49fb5
Kris Jones <kristop.jones@gmail.com>
2021-02-23 13:11:53 -0700

    setup

---
+++ Setup.sql
@@ -0,0 +1,23 @@
+using flowers2171;
+
+-- CREATE TABLE bricks
+-- (
+--   id INT AUTO_INCREMENT,
+--   name VARCHAR(255),
+--   color VARCHAR(255),
+
+--   PRIMARY KEY (id);
+-- );
+
+-- CREATE TABLE kits
+-- (
+--   id INT AUTO_INCREMENT,
+--   name VARCHAR(255),
+--   instructions VARCHAR(255),
+--   price DECIMAL(6,2),
+
+--   PRIMARY KEY (id);
+-- );
+
+-- TRUNCATE TABLE kits;
+-- TRUNCATE TABLE bricks;
